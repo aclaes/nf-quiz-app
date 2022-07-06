61fa5854e73fa79a74975ca2be731838aa157a53
Alexander Claes <hello@aclaes.com>
2022-07-06 16:24:59 +0200

    Add basic component structure

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-// -- card
+import Bookmark from './components/bookmark/bookmark.js';
+import Card from './components/card/card.js';
 
-const cardButtonAnswer = document.querySelector(
-  '[data-js="card-button-answer"]'
-);
-const cardAnswer = document.querySelector('[data-js="card-answer"]');
-
-let isAnswerShown = false;
-
-cardButtonAnswer.addEventListener('click', () => {
-  isAnswerShown = !isAnswerShown;
-
-  if (isAnswerShown) {
-    cardButtonAnswer.innerText = 'Hide answer';
-    cardAnswer.classList.add('card__answer--active');
-  } else {
-    cardButtonAnswer.innerText = 'Show answer';
-    cardAnswer.classList.remove('card__answer--active');
-  }
-});
-
-// -- bookmark
-
-const bookmark = document.querySelector('[data-js="bookmark"]');
-
-let isBookmarked = false;
-
-bookmark.addEventListener('click', () => {
-  isBookmarked = !isBookmarked;
-
-  if (isBookmarked) {
-    bookmark.classList.add('bookmark--active');
-  } else {
-    bookmark.classList.remove('bookmark--active');
-  }
-});
+Bookmark();
+Card();
